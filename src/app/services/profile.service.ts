77c5479a2ffe502b51752051c162621cfc62dda5
Mburiah <bykieswagix5@gmail.com>
2019-12-09 16:39:11 +0300

    added  constructor

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid = '947b5917e35509f9d697';
+  private clientsecret =  '043bfb031bd753a4cc7b39840c62f97f4fa63c5e';
+  http: any;
+
+  constructor(private httpClient) {
+    console.log('service is now ready!');
+    this.username = 'Mburiah';
+   }
+
+   getProfileInfo() {
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid +
+     '&client_secret=' + this.clientsecret) .map(res => res.json());
+    }
 }
