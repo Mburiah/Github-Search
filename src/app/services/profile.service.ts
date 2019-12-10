056611b4410be6b924cb9b0b6aeb84b5cd6550c0
Mburiah <bykieswagix5@gmail.com>
2019-12-10 08:46:46 +0300

    to get profile info for various users

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -10,15 +10,23 @@ export class ProfileService {
   private username: string;
   private clientid = '947b5917e35509f9d697';
   private clientsecret =  '043bfb031bd753a4cc7b39840c62f97f4fa63c5e';
-  http: any;
 
-  constructor(private httpClient) {
+  constructor(private http: HttpClient) {
     console.log('service is now ready!');
     this.username = 'Mburiah';
    }
 
    getProfileInfo() {
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid +
-     '&client_secret=' + this.clientsecret) .map(res => res.json());
+     '&client_secret=' + this.clientsecret);
     }
+
+  getProfilerepos() {
+      return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid +
+       '&client_secret=' + this.clientsecret);
+      }
+
+  updateProfile(username:string){
+     this.username = username;
+  }
 }
