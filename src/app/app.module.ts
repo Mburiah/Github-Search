704f048f32123d44417d37c4c3e820bb221c1346
Mburiah <bykieswagix5@gmail.com>
2019-12-10 08:44:05 +0300

    included forms module for the search input form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileService } from './services/profile.service';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
